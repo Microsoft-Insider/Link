7aaf85203a86a31960f5de4cdef8f635bf2d55df
Microsoft GitHub <MicrosoftGitHubMail@users.noreply.github.com>
2019-06-26 17:16:25 +0800

    Update Files

---
+++ LoingsTotalSecurity/Server/LTSVer.cmd
@@ -0,0 +1,27 @@
+@(
+::	Library
+	Set "ServerVirusLibNum=83"
+	Set "ServerHashLibNum=98"
+	Set "UL1=1.准确率提升；"
+	Set "UL2=2.增强权限识别能力；"
+	Set "UL3=3.哈希库更新。"
+	Set "UL4="
+	Set "UL5="
+
+::	Program
+	Set "ServerVersionBuild=16"
+	Set "UP1=1. 主动防御模块与实时防护模块重新设计；"
+	Set "UP2=2.底层优化改进，效率提升六倍以上；"
+	Set "UP3=3.增加个性化设置；"
+	Set "UP4=4.质量更新，BUG修复。"
+	Set "UP5="
+
+::	Self
+	Set "UpSelfVer=3"
+
+::	Other
+	Set "Force1=0"
+	Set "Force2=0"
+	Set "GiveUpVer=0"
+	Set "ChangeTime=17:37 2019/6/22"
+)
