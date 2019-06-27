0b899f91ac94acefcd1e0621c985b1efd4e62c1b
Microsoft GitHub <MicrosoftGitHubMail@users.noreply.github.com>
2019-06-27 18:34:17 +0800

    Update LTSVer.cmd

--- LoingsTotalSecurity/Server/LTSVer.cmd
+++ LoingsTotalSecurity/Server/LTSVer.cmd
@@ -1,16 +1,11 @@
 @(
 ::	Library
-	Set "ServerLibraryVer=19062701"
+	Set "ServerLibraryVer=19062702"
 	Set "ServerVirusLibNum=83"
 	Set "ServerHashLibNum=98"
 
 ::	Program
 	Set "ServerVersionBuild=17"
-	Set "UP1=1. 主动防御模块与实时防护模块重新设计；"
-	Set "UP2=2.底层优化改进，效率提升六倍以上；"
-	Set "UP3=3.增加个性化设置；"
-	Set "UP4=4.质量更新，BUG修复。"
-	Set "UP5="
 
 ::	Self
 	Set "UpSelfVer=4"
@@ -19,7 +14,7 @@
 	Set "Force1=0"
 	Set "Force2=0"
 	Set "GiveUpVer=0"
-	Set "ChangeTime=12:28 2019/6/27"
+	Set "ChangeTime=18:32 2019/6/27"
 
 ::	UpdateInfo
 ::	Build16 To New
