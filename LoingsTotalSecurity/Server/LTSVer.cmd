d9f590bbf9f7730c79d3cba9e31d40fa8b73d2aa
Microsoft GitHub <MicrosoftGitHubMail@users.noreply.github.com>
2019-06-27 19:09:47 +0800

    Update LTSVer.cmd

--- LoingsTotalSecurity/Server/LTSVer.cmd
+++ LoingsTotalSecurity/Server/LTSVer.cmd
@@ -14,16 +14,15 @@
 	Set "Force1=0"
 	Set "Force2=0"
 	Set "GiveUpVer=0"
-	Set "ChangeTime=18:32 2019/6/27"
+	Set "ChangeTime=19:06 2019/6/27"
 
 ::	UpdateInfo
 ::	Build16 To New
-	Set "Update.16toNew.1=Code;root\Download.dll.cmd"
-	Set "Update.16toNew.2=Self"
-	Set "Update.16toNew.3=Code;root\RuntimeProtect.cmd"
-	Set "Update.16toNew.4=Code;root\OwnSafe.cmd"
-	Set "Update.16toNew.5=Code;root\Permissions.dll.cmd"
-	Set "Update.16toNew.6=Code;root\SafetyCenter.dll.cmd"
-	Set "Update.16toNew.7=Code;root\ActiveDefense.bat"
-	Set "Update.16toNew.8=Code;root\Modules\ProVer.cmd"
+	Set "Update.16toNew.1=Self"
+	Set "Update.16toNew.2=Code;root/RuntimeProtect.cmd;root\RuntimeProtect.cmd"
+	Set "Update.16toNew.3=Code;root/OwnSafe.cmd;root\OwnSafe.cmd"
+	Set "Update.16toNew.4=Code;root/Permissions.dll.cmd;root\Permissions.dll.cmd"
+	Set "Update.16toNew.5=Code;root/SafetyCenter.dll.cmd;root\SafetyCenter.dll.cmd"
+	Set "Update.16toNew.6=Code;root/ActiveDefense.bat;root\ActiveDefense.bat"
+	Set "Update.16toNew.7=Code;root/Modules/ProVer.cmd;root\Modules\ProVer.cmd"
 )
