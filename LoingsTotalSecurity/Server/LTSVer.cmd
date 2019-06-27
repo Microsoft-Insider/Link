a6b99053df5f99679e2d92837d0686ef4d3af9d3
Microsoft GitHub <MicrosoftGitHubMail@users.noreply.github.com>
2019-06-27 18:27:41 +0800

    Update Files

--- LoingsTotalSecurity/Server/LTSVer.cmd
+++ LoingsTotalSecurity/Server/LTSVer.cmd
@@ -1,15 +1,11 @@
 @(
 ::	Library
+	Set "ServerLibraryVer=19062701"
 	Set "ServerVirusLibNum=83"
 	Set "ServerHashLibNum=98"
-	Set "UL1=1.准确率提升；"
-	Set "UL2=2.增强权限识别能力；"
-	Set "UL3=3.哈希库更新。"
-	Set "UL4="
-	Set "UL5="
 
 ::	Program
-	Set "ServerVersionBuild=16"
+	Set "ServerVersionBuild=17"
 	Set "UP1=1. 主动防御模块与实时防护模块重新设计；"
 	Set "UP2=2.底层优化改进，效率提升六倍以上；"
 	Set "UP3=3.增加个性化设置；"
@@ -17,11 +13,22 @@
 	Set "UP5="
 
 ::	Self
-	Set "UpSelfVer=3"
+	Set "UpSelfVer=4"
 
 ::	Other
 	Set "Force1=0"
 	Set "Force2=0"
 	Set "GiveUpVer=0"
-	Set "ChangeTime=17:37 2019/6/22"
+	Set "ChangeTime=12:28 2019/6/27"
+
+::	UpdateInfo
+::	Build16 To New
+	Set "Update.16toNew.1=Code;root\Download.dll.cmd"
+	Set "Update.16toNew.2=Self"
+	Set "Update.16toNew.3=Code;root\RuntimeProtect.cmd"
+	Set "Update.16toNew.4=Code;root\OwnSafe.cmd"
+	Set "Update.16toNew.5=Code;root\Permissions.dll.cmd"
+	Set "Update.16toNew.6=Code;root\SafetyCenter.dll.cmd"
+	Set "Update.16toNew.7=Code;root\ActiveDefense.bat"
+	Set "Update.16toNew.8=Code;root\Modules\ProVer.cmd"
 )
