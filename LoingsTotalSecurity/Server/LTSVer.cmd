ba1f5304ed5c1739674b0b3363c180e0d42e2073
Microsoft GitHub <MicrosoftGitHubMail@users.noreply.github.com>
2019-06-30 13:41:31 +0800

    Update Files

--- LoingsTotalSecurity/Server/LTSVer.cmd
+++ LoingsTotalSecurity/Server/LTSVer.cmd
@@ -21,10 +21,10 @@
 	Set "Update.17toNew.1=Same;16"
 ::	Build16 To New
 	Set "Update.16toNew.1=Self"
-	Set "Update.16toNew.2=Code;root/RuntimeProtect.cmd.rar;root\RuntimeProtect.cmd.rar"
-	Set "Update.16toNew.3=Code;root/OwnSafe.cmd.rar;root\OwnSafe.cmd.rar"
-	Set "Update.16toNew.4=Code;root/Permissions.dll.cmd.rar;root\Permissions.dll.cmd.rar"
-	Set "Update.16toNew.5=Code;root/SafetyCenter.dll.cmd.rar;root\SafetyCenter.dll.cmd.rar"
-	Set "Update.16toNew.6=Code;root/ActiveDefense.bat.rar;root\ActiveDefense.bat.rar"
-	Set "Update.16toNew.7=Code;root/Modules/ProVer.cmd.rar;root\Modules\ProVer.cmd.rar"
+	Set "Update.16toNew.2=Code;root/RuntimeProtect.cmd.rar;root\RuntimeProtect.cmd.rar;root\"
+	Set "Update.16toNew.3=Code;root/OwnSafe.cmd.rar;root\OwnSafe.cmd.rar;root\"
+	Set "Update.16toNew.4=Code;root/Permissions.dll.cmd.rar;root\Permissions.dll.cmd.rar;root\"
+	Set "Update.16toNew.5=Code;root/SafetyCenter.dll.cmd.rar;root\SafetyCenter.dll.cmd.rar;root\"
+	Set "Update.16toNew.6=Code;root/ActiveDefense.bat.rar;root\ActiveDefense.bat.rar;root\"
+	Set "Update.16toNew.7=Code;root/Modules/ProVer.cmd.rar;root\Modules\ProVer.cmd.rar;root\Modules\"
 )
