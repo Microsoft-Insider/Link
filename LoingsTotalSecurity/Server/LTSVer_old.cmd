dae507801bd1a08d07c908512e7527e937f44f4d
Microsoft GitHub <MicrosoftGitHubMail@users.noreply.github.com>
2019-07-21 17:36:27 +0800

    Update Files

---
+++ LoingsTotalSecurity/Server/LTSVer_old.cmd
@@ -0,0 +1,30 @@
+@(
+::Library
+	Set "ServerLibraryVer=19062702"
+	Set "ServerVirusLibNum=83"
+	Set "ServerHashLibNum=98"
+::Program
+	Set "ServerVersionBuild=20"
+::Self
+	Set "UpSelfVer=5"
+::Other
+	Set "Force1=0"
+	Set "Force2=0"
+	Set "GiveUpVer=0"
+	Set "ChangeTime=11:25 2019/7/19"
+::UpdateInfo
+	Set "Update.19toNew.1=Code;root/Uninst.cmd.rar;root\Uninst.cmd.rar;root\"
+	Set "Update.19toNew.2=Code;root/Main.cmd.rar;root\Main.cmd.rar;root\"
+	Set "Update.19toNew.3=Code;root/RuntimeProtect.cmd.rar;root\RuntimeProtect.cmd.rar;root\"
+	Set "Update.19toNew.4=Code;root/Shell.Real.dll.cmd.rar;root\Shell.Real.dll.cmd.rar;root\"
+	Set "Update.19toNew.5=Code;root/Modules/ProVer.cmd.rar;root\Modules\ProVer.cmd.rar;root\Modules\"
+	Set "Update.18toNew.1=Code;root/Scan.cmd.rar;root\Scan.cmd.rar;root\"
+	Set "Update.18toNew.2=Same;19"
+	Set "Update.17toNew.1=Same;16"
+	Set "Update.16toNew.1=Self"
+	Set "Update.16toNew.2=Code;root/OwnSafe.cmd.rar;root\OwnSafe.cmd.rar;root\"
+	Set "Update.16toNew.3=Code;root/Permissions.dll.cmd.rar;root\Permissions.dll.cmd.rar;root\"
+	Set "Update.16toNew.4=Code;root/SafetyCenter.dll.cmd.rar;root\SafetyCenter.dll.cmd.rar;root\"
+	Set "Update.16toNew.5=Code;root/ActiveDefense.bat.rar;root\ActiveDefense.bat.rar;root\"
+	Set "Update.16toNew.6=Same;18"
+)
