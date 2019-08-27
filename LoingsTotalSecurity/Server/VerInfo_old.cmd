295c6189cafa15c3d4266685e9370c8c25e9a2be
Microsoft <Microsoft@microsoft.com>
2019-08-28 00:39:20 +0800

    Update

---
+++ LoingsTotalSecurity/Server/VerInfo_old.cmd
@@ -0,0 +1,13 @@
+@(
+::Library
+	Set "ServerLibraryVer=19081001"
+	Set "ServerVirusLibNum=85"
+	Set "ServerHashLibNum=98"
+::Program
+	Set "ServerVersionBuild=22"
+::Self
+	Set "UpSelfVer=7"
+::Other
+	Set "GiveUpVer=21"
+	Set "ChangeTime=23:51 2019/8/27"
+)
